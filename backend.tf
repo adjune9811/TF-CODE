d1a50f3d84bac3655407435e51570b584e0b5a22
Ajay Rawat <ajayrawat111@gmail.com>
2025-06-27 15:33:46 +0530

    updated storage

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "terraform-storage-rg" //        = "abcdefghijklmnopqrstuvwxyz0123456789..."  # Can also be set via `ARM_ACCESS_KEY` environment variable.
-    storage_account_name = "addsterraformstate201"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "prod-tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name = "terraform-storage-rg" 
+    storage_account_name = "addsterraformstate201"
+    container_name       = "prod-tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
