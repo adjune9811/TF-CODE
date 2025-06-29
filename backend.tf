7878e3e9031662935152857d73a27f4f79a47d71
Ajay Rawat <ajayrawat111@gmail.com>
2025-06-29 21:14:50 +0530

    updated yaml

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name = "terraform-storage-rg" 
     storage_account_name = "addsterraformstate201"
     container_name       = "prod-tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "prodterraform.tfstate"
   }
 }
